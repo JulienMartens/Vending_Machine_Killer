04bbc815de3560a8a76294bf6d5cabe370559c96
JulienMartens <47147929+JulienMartens@users.noreply.github.com>
2023-10-16 22:48:51 +0200

    feat: replace pnj with true body

--- Scenes/PNJ/pnj.gd
+++ Scenes/PNJ/pnj.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 @onready var camera_pnj = $Camera3D
+@onready var camera_pnj_dead = $Camera3DDead
 @onready var player = get_tree().get_root().get_node("World/Player")
 @onready var dialogueBox = player.get_node("Camera/UI/HBoxContainer/Dialogue")
 @onready var nav_agent = $NavigationAgent3D
+@onready var animation_player = $Body/AnimationPlayer
 var triggered_state = 0
 const SPEED = 8
 
@@ -10,7 +12,7 @@ func _ready():
 	player.interact.connect(interact)
 	
 func interact():
-	if triggered_state in [1,4,7]:
+	if triggered_state in [1,4,8]:
 		player.axis_lock_linear_x = false
 		player.axis_lock_linear_y = false
 		player.axis_lock_linear_z = false
@@ -31,12 +33,13 @@ func _process(_delta):
 		set_velocity(new_velocity)
 		look_at(target_location)
 		move_and_slide()
-		$AnimationPlayer.play("walk")
+		animation_player.play("Walking in place")
 		if nav_agent.distance_to_target()<5:
-			$AnimationPlayer.play("RESET")
+			animation_player.play("Idle")
 			triggered_state = 3
 	if triggered_state == 6:
-			$AnimationPlayer.play("dead")
+			animation_player.play("Dying")
+			triggered_state=7
 				
 func _on_area_3d_body_entered(body):
 	if body.name=="Player" and not triggered_state:
@@ -54,11 +57,11 @@ func _on_area_3d_body_entered(body):
 		dialogueBox.text = tr("pnj_poor")
 		get_tree().get_root().get_node("World/Coin").visible = true
 		triggered_state = 4
-	if body.name=="Player" and triggered_state == 6:
+	if body.name=="Player" and triggered_state == 7:
 		player.axis_lock_linear_x = true
 		player.axis_lock_linear_y = true
 		player.axis_lock_linear_z = true
-		camera_pnj.current = true
+		camera_pnj_dead.current = true
 		dialogueBox.text =  tr("pnj_dead")
 		get_tree().get_root().get_node("World/Donuts").visible = true
-		triggered_state = 7
+		triggered_state = 8
