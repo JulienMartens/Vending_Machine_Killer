1ccea957d3edf2d7f99ba2bba066d57466d5b9d1
JulienMartens <47147929+JulienMartens@users.noreply.github.com>
2023-10-19 18:02:30 +0200

    feat: add house jumpscare and main menu animation

---
+++ Levels/House_jumpscare.gd
@@ -0,0 +1,17 @@
+extends Path3D
+
+@onready var player = get_tree().get_root().get_node(get_tree().current_scene.name+"/Player")
+var move_speed = 0
+
+func _physics_process(delta):
+	$PathFollow3D.progress += move_speed * delta
+	if self.visible == false and player.has_key:
+		self.visible = true
+	if $PathFollow3D.progress_ratio == 1:
+		queue_free()
+		
+func _on_area_3d_body_entered(body):
+	if self.visible:
+		$HouseJumpscare.play()
+		move_speed = 5
+
---
+++ Menu/Main Menu/Path3D.gd
@@ -0,0 +1,10 @@
+extends Path3D
+
+var move_speed = 1.5
+
+
+func _ready():
+	$PathFollow3D/evil_vending_machine/AnimationPlayer.stop()
+
+func _physics_process(delta):
+	$PathFollow3D.progress += move_speed * delta
