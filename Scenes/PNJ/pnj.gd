dac090bcffd96d42db5257ab7a3a59ffb5e3d27e
JulienMartens <47147929+JulienMartens@users.noreply.github.com>
2023-10-21 13:55:40 +0200

    chore: introduce timers and theme for pnj chat

--- Scenes/PNJ/pnj.gd
+++ Scenes/PNJ/pnj.gd
@@ -11,19 +11,27 @@ extends CharacterBody3D
 var triggered_state = 0
 const SPEED = 8
 
-func _ready():
-	player.interact.connect(interact)
-	
-func interact():
-	if triggered_state in [1,4,9]:
-		player.axis_lock_linear_x = false
-		player.axis_lock_linear_y = false
-		player.axis_lock_linear_z = false
-		player.get_node("Camera").current = true
-		dialogueBox.text = ""
-		triggered_state += 1
+func give_back_camera():
+	player.axis_lock_linear_x = false
+	player.axis_lock_linear_y = false
+	player.axis_lock_linear_z = false
+	player.get_node("Camera").current = true
+	dialogueBox.text = ""
+	triggered_state += 1
+
+func take_camera(camera="standard"):
+	player.axis_lock_linear_x = true
+	player.axis_lock_linear_y = true
+	player.axis_lock_linear_z = true
+	if camera=="standard":
+		camera_pnj.current = true
+	elif camera=="dead":
+		camera_pnj_dead.current = true
+	elif camera=="kill":
+		$Camera3DAnim.current = true
 
 func _process(_delta):
+	print_debug(triggered_state)
 	if triggered_state in [0,3,4]:
 		look_at(player.global_transform.origin)
 	if triggered_state == 2:
@@ -46,47 +54,44 @@ func _process(_delta):
 
 func _on_area_3d_body_entered(body):
 	if body.name=="Player" and not triggered_state:
-		player.axis_lock_linear_x = true
-		player.axis_lock_linear_y = true
-		player.axis_lock_linear_z = true
-		camera_pnj.current = true
+		take_camera()
 		dialogueBox.text = tr("pnj_greet")
 		triggered_state = 1
+		await get_tree().create_timer(5).timeout
+		give_back_camera()
 	if body.name=="Player" and triggered_state == 3:
-		player.axis_lock_linear_x = true
-		player.axis_lock_linear_y = true
-		player.axis_lock_linear_z = true
-		camera_pnj.current = true
+		take_camera()
 		dialogueBox.text = tr("pnj_poor")
 		get_tree().get_root().get_node("World/Coin").visible = true
 		triggered_state = 4
+		await get_tree().create_timer(8).timeout
+		give_back_camera()
 	if body.name=="Player" and triggered_state == 8:
-		player.axis_lock_linear_x = true
-		player.axis_lock_linear_y = true
-		player.axis_lock_linear_z = true
-		camera_pnj_dead.current = true
+		var camera="dead"
+		take_camera(camera)
 		dialogueBox.text =  tr("pnj_dead")
 		get_tree().get_root().get_node("World/Donuts").visible = true
 		triggered_state = 9
+		await get_tree().create_timer(5).timeout
+		give_back_camera()
 
 func death_animation():
-	if triggered_state == 7:
-		normalVendingMachine.queue_free()
-		VendingMachine.visible = true
-		$Camera3DAnim.current = true
-		dialogueBox.text = tr("pnj_wait")
-		await get_tree().create_timer(5).timeout
-		dialogueBox.text = tr("pnj_steal")
-		await get_tree().create_timer(8).timeout
-		dialogueBox.text = ""
-		VendingMachine_animation_player.play("eyes out")
-		$evil_vending_machine/ShortScaryPlayer.play()
-		await get_tree().create_timer(2).timeout
-		$evil_vending_machine/KillMusicPlayer.play()
-		VendingMachine_animation_player.play("kill")
-		await get_tree().create_timer(0.6).timeout
-		animation_player.play("Dying")
-		await get_tree().create_timer(2.3).timeout
-		triggered_state = 8
-		player.get_node("Camera").current = true
-		VendingMachine.visible = false
+	normalVendingMachine.queue_free()
+	VendingMachine.visible = true
+	var camera="kill"
+	take_camera(camera)
+	dialogueBox.text = tr("pnj_wait")
+	await get_tree().create_timer(5).timeout
+	dialogueBox.text = tr("pnj_steal")
+	await get_tree().create_timer(8).timeout
+	dialogueBox.text = ""
+	VendingMachine_animation_player.play("eyes out")
+	$evil_vending_machine/ShortScaryPlayer.play()
+	await get_tree().create_timer(2).timeout
+	$evil_vending_machine/KillMusicPlayer.play()
+	VendingMachine_animation_player.play("kill")
+	await get_tree().create_timer(0.6).timeout
+	animation_player.play("Dying")
+	await get_tree().create_timer(2.3).timeout
+	give_back_camera()
+	VendingMachine.visible = false
